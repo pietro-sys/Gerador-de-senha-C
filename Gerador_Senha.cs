fd299e24ce2aaed551148d94e870cd60b9958b5b
Pietro <pietrofernandesstefanuto@gmail.com>
2026-05-21 21:32:02 -0300

    adiciona fluxo de continuação ao gerador
    Ao finalizar a geração de uma senha, o usuário agora é perguntado
    se deseja gerar outra senha ou encerrar o programa.
    
    Substituído o "Pressione Enter para continuar" por uma escolha
    explícita de (s/n), dando mais controle ao usuário sobre o fluxo
    do programa. Adicionado Environment.Exit(0) para encerrar
    completamente ao escolher sair.

--- Gerador_Senha.cs
+++ Gerador_Senha.cs
@@ -4,20 +4,66 @@ class Gerador_Senha
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Gerador de Senha");
-        Console.WriteLine("Digite o comprimento da senha:");
-        int comprimento = int.Parse(Console.ReadLine());
-
-        string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-        Random random = new Random();
-        string senha = "";
-
-        for (int i = 0; i < comprimento; i++)
+        while (true)
         {
-            int index = random.Next(caracteres.Length);
-            senha += caracteres[index];
-        }
+            Console.Clear();
+            Console.WriteLine("Gostaria de gerar uma senha? (s/n)");
+            string resposta = Console.ReadLine().Trim();
 
-        Console.WriteLine("Senha gerada: " + senha);
+            if (resposta == "n")
+            {
+                Console.WriteLine("Saindo do programa. Até mais!");
+                break;
+            }
+            else if (resposta == "s")
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("Gerador de Senha");
+                        Console.WriteLine("Digite o comprimento da senha:");
+                        int comprimento = int.Parse(Console.ReadLine());
+
+                        string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+                        Random random = new Random();
+                        string senha = "";
+
+                        for (int i = 0; i < comprimento; i++)
+                        {
+                            int index = random.Next(caracteres.Length);
+                            senha += caracteres[index];
+                        }
+
+                        Console.WriteLine("Senha gerada: " + senha);
+                        Console.WriteLine("\nDeseja gerar outra senha? (s/n)");
+                        string continuar = Console.ReadLine().Trim();
+                        if (continuar == "n")
+                        {
+                            Console.WriteLine("Saindo do programa. Até mais!");
+                            Environment.Exit(0);
+                        }
+                        if (continuar == "s")
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Opção inválida. Saindo do programa.");
+                            Environment.Exit(0);
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida. Digite 's' ou 'n'.");
+                Console.ReadKey(true);
+            }
+        }
     }
 }
